5ace3a9805097d8895df854baeef1969fe0851ac
Xclouds <115176022+clouds440@users.noreply.github.com>
2024-08-31 12:10:39 +0000

    Added responsiveness to the logo. We still need a logo tho

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,13 @@
 const Logo = ({ classes, fontSize }) => {
   return (
-    <div className={`flex ${classes} select-none`}>
+    <div className={`flex select-none items-center justify-center ${classes}`}>
       <a href="#home" className="flex">
-        <h1 className={`font-bold ${fontSize} ml-2 text-cyan-600`}>Tailor</h1>
-        <h1 className={`font-bold ${fontSize} text-yellow-500`}>Ease</h1>
+        <h1 className={`font-bold ${fontSize} text-cyan-600`}>
+          T<span className="hidden md:inline-block">ailor</span>
+        </h1>
+        <h1 className={`font-bold ${fontSize} text-yellow-500`}>
+          E<span className="hidden md:inline-block">ase</span>
+        </h1>
       </a>
     </div>
   );
