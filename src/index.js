2dfbaaa525ef2437933998a3e10a5a2f3857b615
Xclouds <115176022+clouds440@users.noreply.github.com>
2024-11-24 21:48:07 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import { UserProvider } from "./utils/UserContext";
 import PopupMessage from "./components/PopupMessage";
 import { DirectionProvider } from "./utils/LayoutDirectionContext";
 import "./utils/LanguageSwitcher";
-
+import ChatBot from "./components/ChatBot";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <UserProvider>
       <DirectionProvider>
+        <ChatBot />
         <PopupMessage />
         <App />
       </DirectionProvider>
